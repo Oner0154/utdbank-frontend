d017ed8f723a9e7070097102e0313c3a2e8c5e12
Oner <tahirugur2628@gmail.com>
2022-01-12 08:47:52 +0100

    day1

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import TopBar from "../components/common/TopBar";
+import Banner from "../components/home/Banner";
 
 const HomePage = () => {
   return (
     <>
       <TopBar />
-      
+      <Banner />
     </>
   );
 };
